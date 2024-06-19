e19eca0a0cc6f0d596d27cc1218e6c681e9aaec2
kspriyaranjan <priyaranjan.ks@hotmail.com>
2024-06-19 18:54:30 +0530

    Update

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -4,10 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
   if (scrollLink) {
     scrollLink.addEventListener('click', function(event) {
       event.preventDefault();
-      const href = scrollLink.getAttribute('href');  // Fetch href attribute
-      if (href && href.startsWith('#')) {  // Check if href starts with '#'
-        const targetId = href.substring(1);  // Remove the '#'
-        const targetElement = document.getElementById(targetId);  // Get the element by ID
+      let href = scrollLink.getAttribute('href');
+      console.log('Button href:', href);  // Debug: Log the href value
+
+      // Remove backslash if present and check if href starts with '#'
+      href = href.replace(/\\/g, '');
+      if (href && href.startsWith('#')) {
+        const targetId = href.substring(1);
+        console.log('Target ID:', targetId);  // Debug: Log the target ID
+
+        const targetElement = document.getElementById(targetId);
+        console.log('Target Element:', targetElement);  // Debug: Log the found target element
 
         if (targetElement) {
           window.scrollTo({
